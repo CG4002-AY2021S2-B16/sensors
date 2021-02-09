f54f4fad67e6fd18e4d41218496b01cb555e0a8c
elstonayx <elston.ayx@u.nus.edu>
2021-02-10 01:01:45 +0800

    Update calibration code

--- src/main.ino
+++ src/main.ino
@@ -5,29 +5,10 @@
 #include "MPU6050.h"
 #include "CommsInt.h"
 
-// CONSTANTS bluno 3
-#define X_GYRO_OFFSET -792 //-976
-#define Y_GYRO_OFFSET -2441 // -6527
-#define Z_GYRO_OFFSET 1544 //988
-#define X_ACCEL_OFFSET 143 //-52
-#define Y_ACCEL_OFFSET 5 // -17
-#define Z_ACCEL_OFFSET 33 //-24
-
 MPU6050 imuSensor;
 
 int16_t accelX, accelY, accelZ;
 int16_t gyroX, gyroY, gyroZ;
-
-
-void setIMUSensorOffset()
-{
-  imuSensor.setXGyroOffset(X_GYRO_OFFSET);
-  imuSensor.setYGyroOffset(Y_GYRO_OFFSET);
-  imuSensor.setZGyroOffset(Z_GYRO_OFFSET);
-  imuSensor.setXAccelOffset(X_ACCEL_OFFSET);
-  imuSensor.setYAccelOffset(Y_ACCEL_OFFSET);
-  imuSensor.setZAccelOffset(Z_ACCEL_OFFSET);
-}
 
 void setup()
 {
@@ -42,8 +23,10 @@ void setup()
   {
     Serial.println("MPU6050 connection failed!");
   }
-}
 
+  imuSensor.CalibrateAccel(10);
+  imuSensor.CalibrateGyro(10);
+}
 
 void loop()
 {
@@ -59,6 +42,7 @@ void loop()
   else if (handshake_done)
   {
     imuSensor.getMotion6(&accelX, &accelY, &accelZ, &gyroX, &gyroY, &gyroZ);
+    // Add data smoothing/function to reduce noise here
     dataResponse(accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
   }
 
