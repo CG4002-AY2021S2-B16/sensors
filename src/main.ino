66fa4baa95a2553b57b761a80f2bed60195a1a4b
Elston Aw <elston.ayx@u.nus.edu>
2021-02-22 10:23:13 +0800

    Integrate encryption and updated sensor code (#12)
    * Integrate code
    
    * Comment out motion sensor code

--- src/main.ino
+++ src/main.ino
@@ -7,11 +7,22 @@
 
 #define ONBOARD_LED 13
 #define EMG_PIN A0
+#define IMU_INT_PIN A1
+
+#define ACCEL_RANGE 2 // +- 8g for range from +32768/-32767
+#define GYRO_RANGE 2
+
+#define RAW_TO_MS_2 (float)__SHRT_MAX__ * 9.81 * 8
+#define RAW_TO_DEG_S_2 (float)__SHRT_MAX__ * 1000.
+
+#define ZERO_MOTION_THRESHOLD 8 // threshold = value * 2m
+#define ZERO_MOTION_DURATION 5  // duration = value * 64ms
 
 MPU6050 imuSensor;
 
 int16_t accelX, accelY, accelZ;
 int16_t gyroX, gyroY, gyroZ;
+bool isMotionDetected;
 
 void setup()
 {
@@ -26,23 +37,37 @@ void setup()
   prepareAES();
 
   imuSensor.initialize();
-  if (!imuSensor.testConnection())
+  if (imuSensor.testConnection())
   {
-    Serial.println("MPU6050 connection failed!");
-  }
-  else
-  {
-    imuSensor.CalibrateAccel(10);
-    imuSensor.CalibrateGyro(10);
+    // Calibrate IMU with 600 readings
+    imuSensor.CalibrateAccel(6);
+    imuSensor.CalibrateGyro(6);
+    // Set IMU range
+    imuSensor.setFullScaleAccelRange(ACCEL_RANGE);
+    imuSensor.setFullScaleGyroRange(GYRO_RANGE);
+    // Set zero motion detection
+    imuSensor.setZeroMotionDetectionThreshold(ZERO_MOTION_THRESHOLD);
+    imuSensor.setZeroMotionDetectionDuration(ZERO_MOTION_DURATION);
+
     digitalWrite(ONBOARD_LED, LOW);
   }
+  else // Error connecting with MPU
+  {
+    for (;;)
+    {
+      digitalWrite(ONBOARD_LED, HIGH);
+      delay(500);
+      digitalWrite(ONBOARD_LED, LOW);
+      delay(500);
+    }
+  }
 
-  Serial.begin(115200);
   delay(1000);
 }
 
 void loop()
 {
+  isMotionDetected = !imuSensor.getZeroMotionDetected();
   receiveData();
 
   if (new_handshake_req)
@@ -55,8 +80,10 @@ void loop()
   else if (handshake_done)
   {
     imuSensor.getMotion6(&accelX, &accelY, &accelZ, &gyroX, &gyroY, &gyroZ);
-    // Add data smoothing/function to reduce noise here
+    // if (isMotionDetected)
+    // {
     dataResponse(accelX, accelY, accelZ, gyroX, gyroY, gyroZ);
+    // }
   }
 
   delay(20);
