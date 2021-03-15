5f619d5cbd3df35b7efd5a6df316d95ef2624776
Elston Aw <elston.ayx@u.nus.edu>
2021-03-15 18:07:20 +0800

    Add emg code (#11)
    * Add encryption code + makefile configg
    
    * Remove examples folder
    
    * Refresh libraries
    
    * Make modifications
    
    * Integrate code
    
    * Implement liveness packet
    
    * Resolve conflicts
    
    * Resolve conflicts
    
    * Change zero motion duration
    
    * Add emg data transmission code
    
    * Set false
    
    * Remove unused exports
    
    * Add EMG and FFT code
    
    * WIP
    
    * Add separate file for EMG production code
    
    Co-authored-by: rssujay <rssujay97@gmail.com>

---
+++ lib/EMG/EMG.cpp
@@ -0,0 +1,98 @@
+/**
+ * To find out the fatigue of a user, we are using the following research papers:
+ * https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6679263/
+ * 
+ * The papers uses the following values to determine muscular fatigue:
+ * - Mean Absolute Value of sensor reading (MAV)
+ * - Root Mean Square Value of sensor reading (RMS)
+ * - Mean Frequency (MNF)
+ * 
+ * The indicators of muscle fatigue would be shown by the reduction of average
+ * frequency, and the increment of MAV and RMS.
+ */
+
+#include "Arduino.h"
+#include "EMG.h"
+#include "arduinoFFT.h"
+
+#define MAX_VOLTAGE 5.0
+#define ANALOG_SIZE 1023.0
+
+double vReal[EMG_SAMPLE_SIZE];
+double vImag[EMG_SAMPLE_SIZE];
+double freqArr[EMG_SAMPLE_SIZE / 2];
+
+uint16_t sampleCount = 0;
+double totalSensorVal = 0;
+double totalSquaredSensorVal = 0;
+double maxSensorVal = 0;
+
+arduinoFFT FFT = arduinoFFT();
+
+void readEMGData(uint16_t sensorReading)
+{
+  float actualSensorVal = convertSensorValue(sensorReading);
+
+  vReal[sampleCount] = sensorReading;
+  vImag[sampleCount] = 0;
+
+  totalSensorVal += actualSensorVal;
+  totalSquaredSensorVal += (actualSensorVal * actualSensorVal);
+  maxSensorVal = (actualSensorVal > maxSensorVal)
+                     ? actualSensorVal
+                     : maxSensorVal;
+
+  ++sampleCount;
+}
+
+void processEMG(double *maxSensorValue, double *meanSensorValue,
+                double *rmsSensorValue, double *meanSensorFrequency)
+{
+  *meanSensorFrequency = computeFFT();
+  *meanSensorValue = totalSensorVal / (EMG_SAMPLE_SIZE * 1.0);
+  *rmsSensorValue = sqrt(totalSquaredSensorVal / (EMG_SAMPLE_SIZE * 1.0));
+  *maxSensorValue = maxSensorVal;
+
+  resetValues();
+}
+
+void resetValues()
+{
+  maxSensorVal = 0;
+  totalSensorVal = 0;
+  totalSquaredSensorVal = 0;
+  sampleCount = 0;
+}
+
+void setupFrequencyArr()
+{
+  for (int i = 0; i < EMG_SAMPLE_SIZE / 2; i++)
+  {
+    freqArr[i] = (EMG_SAMPLE_FREQUENCY * i) / (EMG_SAMPLE_SIZE * 1.0);
+  }
+}
+
+float convertSensorValue(uint16_t sensorReading)
+{
+  return (sensorReading / ANALOG_SIZE) * MAX_VOLTAGE;
+}
+
+double computeFFT()
+{
+  FFT.Windowing(vReal, EMG_SAMPLE_SIZE, FFT_WIN_TYP_HAMMING, FFT_FORWARD);
+  FFT.Compute(vReal, vImag, EMG_SAMPLE_SIZE, FFT_FORWARD);
+  FFT.ComplexToMagnitude(vReal, vImag, EMG_SAMPLE_SIZE);
+
+  long freqAndPowerSpecSum = 0;
+  long powerSpecSum = 0;
+
+  for (int i = 0; i < EMG_SAMPLE_SIZE / 2; i++)
+  {
+    unsigned int powerSpec = vReal[i] * vReal[i];
+    freqAndPowerSpecSum += powerSpec * freqArr[i];
+    powerSpecSum += powerSpec;
+  }
+
+  double calculatedMAF = (freqAndPowerSpecSum * 1.0) / (powerSpecSum * 1.0);
+  return calculatedMAF;
+}
---
+++ src/emg_main/main.ino
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include "Wire.h"
+
+#include "CommsInt.h"
+
+#include "arduinoFFT.h"
+#include "EMG.h"
+
+#define ONBOARD_LED 13
+#define EMG_PIN A0
+
+double maxSensorReading;
+double meanSensorReading;
+double rmsSensorReading;
+double meanSensorFreq;
+
+void setup()
+{
+  // Initialize the i2c wire connection
+  pinMode(ONBOARD_LED, OUTPUT);
+  pinMode(EMG_PIN, INPUT);
+
+  // Indicate the start of initializing
+  digitalWrite(ONBOARD_LED, HIGH);
+  Wire.begin();
+  prepareAES();
+
+  setupFrequencyArr();
+
+  // Indicate the end of initializing
+  digitalWrite(ONBOARD_LED, LOW);
+}
+
+void loop()
+{
+  readEMGData(analogRead(EMG_PIN));
+  if (new_handshake_req)
+  {
+    handshakeResponse();
+    resetTimeOffset();
+    handshake_done = true;
+    delay(500);
+  }
+  else if (handshake_done)
+  {
+    if (checkLivenessPacketRequired())
+    {
+      livenessResponse();
+    }
+    else if (sampleCount >= EMG_SAMPLE_SIZE)
+    {
+      processEMG(&maxSensorReading, &meanSensorReading, &rmsSensorReading, &meanSensorFreq);
+      EMGdataResponse((float)meanSensorReading, (float)rmsSensorReading, (float)meanSensorFreq);
+      delay(128); // There should not be more than a (small << 128) delay on integrated code, because delay also comes from EMG sampling.
+    }
+  }
+}
